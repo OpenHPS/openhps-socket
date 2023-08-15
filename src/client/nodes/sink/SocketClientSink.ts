ed89cc68a2031247881ffb1b9f3b9a30dcdd70e7
Maxim Van de Wynckel <maxim.van.de.wynckel@vub.be>
2023-08-15 23:30:46 +0200

    fix: serialization for clients

--- src/client/nodes/sink/SocketClientSink.ts
+++ src/client/nodes/sink/SocketClientSink.ts
@@ -1,4 +1,4 @@
-import { DataFrame, RemoteSinkNode, SinkNodeOptions } from '@openhps/core';
+import { DataFrame, RemoteSinkNode, RemoteSinkNodeOptions } from '@openhps/core';
 import { SocketClient } from '../../service';
 import { SocketClientNode } from '../SocketClientNode';
 
@@ -6,7 +6,7 @@ import { SocketClientNode } from '../SocketClientNode';
  * @category Client
  */
 export class SocketClientSink<In extends DataFrame> extends RemoteSinkNode<In, SocketClient> {
-    constructor(options?: SinkNodeOptions) {
+    constructor(options?: RemoteSinkNodeOptions<SocketClient>) {
         super(options);
         this.remoteNode = new SocketClientNode(options);
     }
--- src/client/nodes/source/SocketClientSource.ts
+++ src/client/nodes/source/SocketClientSource.ts
@@ -1,4 +1,4 @@
-import { DataFrame, RemoteSourceNode, SourceNodeOptions } from '@openhps/core';
+import { DataFrame, RemoteSourceNode, RemoteSourceNodeOptions } from '@openhps/core';
 import { SocketClient } from '../../service';
 import { SocketClientNode } from '../SocketClientNode';
 
@@ -6,7 +6,7 @@ import { SocketClientNode } from '../SocketClientNode';
  * @category Client
  */
 export class SocketClientSource<Out extends DataFrame> extends RemoteSourceNode<Out, SocketClient> {
-    constructor(options?: SourceNodeOptions) {
+    constructor(options?: RemoteSourceNodeOptions<SocketClient>) {
         super(options);
         this.remoteNode = new SocketClientNode(options);
         this.on('localerror', console.log);
